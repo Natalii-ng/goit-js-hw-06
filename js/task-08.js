0ccaa6dbc280f6c2c6d15d6800138b243880809b
natalia <geracimova.ng@gmail.ru>
2022-12-21 15:27:59 +0200

    f

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
 
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    if (mail.length === 0 || password.length === 0) {
+        alert( "Заповнені не вси поля!");
+        return;
+    }
+    const formData = {
+        mail,
+        password,
+    };
+
+    console.log(formData);
+    form.reset();
+}
