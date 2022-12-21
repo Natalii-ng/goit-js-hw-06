27f9365fe7e635b2f44d3226f488d161e134e0cd
natalia <geracimova.ng@gmail.ru>
2022-12-21 15:56:04 +0200

    a

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refButton = document.querySelector('.change-color');
+const refColor = document.querySelector('.color');
+const body = document.body;
+
+refButton.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  let changeColor = getRandomHexColor();
+  body.style.backgroundColor = changeColor;
+  refColor.textContent = changeColor;
+
+};
