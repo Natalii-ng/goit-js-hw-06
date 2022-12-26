5135f9a2484a2ebfc03b1d66ed849a2d21590522
natalia <geracimova.ng@gmail.ru>
2022-12-26 15:00:48 +0200

    a

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,8 @@
 const navLiEl = document.querySelectorAll('.item');
 console.log('Number of categories:', navLiEl.length);
-const liTitleEl = document.querySelectorAll('.item h2');
-const ulTitleEl = document.querySelectorAll('.item ul');
-liTitleEl.forEach(element => {
-    console.log('Category:', element.textContent);
+
+navLiEl.forEach(element => {
+    console.log('Category:', element.firstElementChild.textContent);
+    console.log('Elements:', element.lastElementChild.childElementCount);
 })
-ulTitleEl.forEach(element => {
-    console.log('Elements:', element.children.length);
-})
+
